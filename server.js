db286a006547aac257fd19360f3907dc9e236b2e
Melisha <melisha.evans06@gmail.com>
2023-08-27 09:51:37 -0700

    created server.js file

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+//Initializing the app and creating a PORT
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/', apiRoutes);
+app.use('/', htmlRoutes);
+//Starting the server on the PORT
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
