522fa381b92bb08ad7e510fa988234904f97dc4f
Melisha <melisha.evans06@gmail.com>
2023-08-30 22:57:09 -0700

    updated server.js -realized I didn't use /api in my routes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use('/', apiRoutes);
+app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 //Starting the server on the PORT
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
